c62e6a64d404071f92758a76901b5fc856d8986b
AndersenGAV <Andersengal@gmail.com>
2024-01-13 14:54:56 +0200

    DataContext filled

--- DataAccess/Contexts/FileDataContext.py
+++ DataAccess/Contexts/FileDataContext.py
@@ -1,16 +1,16 @@
 class DataContext:
-    _instance = None
+    __instance = None
 
     def __new__(cls):
         if cls._instance is None:
             cls._instance = super(DataContext, cls).__new__(cls)
         return cls._instance
 
-    def read_data(self, filename):
-        with open(filename, 'r') as file:
-            data = file.read()
-        return data
-
-    def write_data(self, filename, data):
-        with open(filename, 'w') as file:
-            file.write(data)
+    # def read_data(self, filename):
+    #     with open(filename, 'r') as file:
+    #         data = file.read()
+    #     return data
+    #
+    # def write_data(self, filename, data):
+    #     with open(filename, 'w') as file:
+    #         file.write(data)
