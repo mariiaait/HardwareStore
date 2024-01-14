21a9686e972b3fe5891c3cab0379ec3a8c043ecf
sergbash1991 <sergbash1991@gmail.com>
2024-01-14 18:16:43 +0200

    Added method Update and custom method get_by_id

--- DataAccess/Repositories/MaterialFileRepository.py
+++ DataAccess/Repositories/MaterialFileRepository.py
@@ -14,4 +14,25 @@ class MaterialFileRepository:
             return json.load(file)
 
     def get_by_id(self, id):
-        return filter(lambda x: x[id] == id, self.get()["materials"])
+        return next(filter(lambda x: x[id] == id, self.get()["materials"]))
+
+    def update(self, data):
+        previous = self.get()
+        MaterialFileRepository.__union(previous, data)
+        self.add(previous)
+
+    @staticmethod
+    def __union(data1, data2):
+        for key, value in data1.items():
+            for item in value:
+                if not MaterialFileRepository.__includes(item, data2[key]):
+                    data1[key].append(item)
+
+    @staticmethod
+    def __includes(data1, data2):
+        for item in data2:
+            _, *current_values = item.values()
+            _, *data1_values = data1.values()
+            if current_values == data1_values:
+                return True
+        return False
