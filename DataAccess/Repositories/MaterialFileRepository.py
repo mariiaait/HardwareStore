797234205b89dca982f9d756af40455742b9911c
shana <bashkatovahelga@gmail.com>
2024-01-15 10:52:15 +0100

    Fix bag in method add

--- DataAccess/Repositories/MaterialFileRepository.py
+++ DataAccess/Repositories/MaterialFileRepository.py
@@ -6,12 +6,19 @@ class MaterialFileRepository:
         self._context = context
 
     def add(self, data):
-        with open(self._context.path, "a", encoding="utf-8") as file:
-            json.dump(data, file, indent=3)
+        try:
+            current_data = self.get()
+            current_data["materials"].append(data)
+            with open(self._context.path, "w", encoding="utf-8") as file:
+                json.dump(current_data, file, indent=3)
+        except FileNotFoundError:
+            with open(self._context.path, "w", encoding="utf-8") as file:
+                json.dump({"materials": [data]}, file, indent=3)
 
     def get(self):
         with open(self._context.path, "r", encoding="utf-8") as file:
             return json.load(file)
 
     def get_by_id(self, id):
-        return filter(lambda x: x[id] == id, self.get()["materials"])
+        result = next(map(lambda x: x[id], filter(lambda x: x[id] == id, self.get()["materials"])))
+        return result
