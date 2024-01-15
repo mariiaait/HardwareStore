c80b0e5899b3abcb253417d36e164bdce2fab806
Kate <frantik4446@gmail.com>
2024-01-15 22:32:41 +0200

    delete method added

--- BusinessLogic/Services/MaterialFileService.py
+++ BusinessLogic/Services/MaterialFileService.py
@@ -34,4 +34,3 @@ class MaterialFileService:
             return self._repository.delete(id)
         except FileNotFoundError('The file has been moved to another directory or does not exist'):
             pass
-
