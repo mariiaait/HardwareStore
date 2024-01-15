8d4aee7988e507e67ef045375e122c732ed7d593
shana <bashkatovahelga@gmail.com>
2024-01-15 16:54:04 +0100

    Fix bag in method add and get method

--- DataAccess/Repositories/MaterialFileRepository.py
+++ DataAccess/Repositories/MaterialFileRepository.py
@@ -6,19 +6,18 @@ class MaterialFileRepository:
         self._context = context
 
     def add(self, data):
-        try:
             current_data = self.get()
             current_data["materials"].append(data)
             with open(self._context.path, "w", encoding="utf-8") as file:
                 json.dump(current_data, file, indent=3)
-        except json.JSONDecodeError:
-            with open(self._context.path, "w", encoding="utf-8") as file:
-                json.dump({"materials": [data]}, file, indent=3)
 
 
     def get(self):
-        with open(self._context.path, "r", encoding="utf-8") as file:
+        try:
+            with open(self._context.path, "r", encoding="utf-8") as file:
             return json.load(file)
+        except json.JSONDecodeError:
+            json.dump({"materials": []}, file, indent=3)
 
     def get_by_id(self, id):
         result = next(map(lambda x: x[id], filter(lambda x: x[id] == id, self.get()["materials"])))
