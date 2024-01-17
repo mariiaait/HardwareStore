f1f19d74a5ffe82c62666289a5378c9a865c8cb2
sergbash1991 <sergbash1991@gmail.com>
2024-01-18 01:08:24 +0200

    Fixed method try_handle_log_levels (decorator)

--- Infrastructure/Validators/Decorators/FileExceptionHandlers.py
+++ Infrastructure/Validators/Decorators/FileExceptionHandlers.py
@@ -8,14 +8,14 @@ def try_handle_log_levels(log_mes):
         def wrapper(*args, **kwargs):
             try:
                 result = func(*args, **kwargs)
-                logger.info(f"INFO: {datetime.datetime.now()}\t{log_mes}")
+                logger.info(f"INFO: {datetime.datetime.now()}\t{log_mes}\tValues: {args}, {kwargs}")
                 if func.__name__ not in ("add", "update", "delete"):
                     return result
             except FileNotFoundError as ex:
-                logger.error(f"ERROR: {datetime.datetime.now()}\t{log_mes}\t{ex}")
+                logger.error(f"ERROR: {datetime.datetime.now()}\t{ex}\tValues: {args}, {kwargs}")
             except json.JSONDecodeError as ex:
-                logger.error(f"ERROR: {datetime.datetime.now()}\t{log_mes}\t{ex}")
+                logger.error(f"ERROR: {datetime.datetime.now()}\t{ex}\tValues: {args}, {kwargs}")
             except Exception as ex:
-                logger.error(f"ERROR: {datetime.datetime.now()}\t{log_mes}\t{ex}")
+                logger.error(f"ERROR: {datetime.datetime.now()}\t{ex}\tValues: {args}, {kwargs}")
         return wrapper
     return decorator
