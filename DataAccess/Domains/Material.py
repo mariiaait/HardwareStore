f340acd875ecdb3627bc6c6ad1785a0635b79725
Kate <frantik4446@gmail.com>
2024-01-18 14:43:29 +0200

    Materials add method __getattribute_fix bug

--- DataAccess/Domains/Material.py
+++ DataAccess/Domains/Material.py
@@ -34,9 +34,9 @@ class Material:
 
         object.__setattr__(self, key, value)
 
+
     def __getattribute__(self, item):
-        atributes_list = ['_id', '_name', '_price']
-        if item in atributes_list:
-            return f'Access to private attribute {item}'
+        if item in ('_id', '_name', '_price'):
+            return AttributeError
         return super().__getattribute__(item)
 
