b150bc13a4f777a62eb00a32cb4cf3aaa1b5c327
Kate <frantik4446@gmail.com>
2024-01-19 21:56:41 +0200

    Service refactor with decorator

--- BusinessLogic/Services/MaterialFileService.py
+++ BusinessLogic/Services/MaterialFileService.py
@@ -1,36 +1,29 @@
 from DataAccess.Repositories import MaterialFileRepository
+from Infrastructure.Validators.Decorators import try_handle_log_levels
 
+import datetime
 
 class MaterialFileService:
     def __init__(self, repository: MaterialFileRepository):
         self._repository = repository
 
+    @try_handle_log_levels(log_mes='ERROR:{0}'.format(datetime.datetime.now()))
     def add(self, data):
-        try:
-            self._repository.add(data)
-        except FileNotFoundError:
-            pass
+        self._repository.add(data)
 
+    @try_handle_log_levels(log_mes='ERROR:{0}'.format(datetime.datetime.now()))
     def get(self):
-        try:
-            return self._repository.get()
-        except FileNotFoundError:
-            pass
+        return self._repository.get()
 
+    @try_handle_log_levels(log_mes='ERROR:{0}'.format(datetime.datetime.now()))
     def get_by_id(self, id):
-        try:
-            return self._repository.get_by_id(id)
-        except FileNotFoundError:
-            pass
+        return self._repository.get_by_id(id)
 
+    @try_handle_log_levels(log_mes='ERROR:{0}'.format(datetime.datetime.now()))
     def update(self, id, data):
-        try:
-            self._repository.update(id, data)
-        except FileNotFoundError:
-            pass
+        self._repository.update(id, data)
 
+    @try_handle_log_levels(log_mes='ERROR:{0}'.format(datetime.datetime.now()))
     def delete(self, id):
-        try:
-            self._repository.delete(id)
-        except FileNotFoundError:
-            pass
+        self._repository.delete(id)
+
