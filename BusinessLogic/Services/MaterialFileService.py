0477daedb8f4572d60a646766f234da25d28161a
Kate <frantik4446@gmail.com>
2024-01-20 12:01:32 +0200

    refactor text in decorator

--- BusinessLogic/Services/MaterialFileService.py
+++ BusinessLogic/Services/MaterialFileService.py
@@ -7,23 +7,23 @@ class MaterialFileService:
     def __init__(self, repository: MaterialFileRepository):
         self._repository = repository
 
-    @try_handle_log_levels(log_mes='ERROR:{0}'.format(datetime.datetime.now()))
+    @try_handle_log_levels('Try to add data to file: ')
     def add(self, data):
         self._repository.add(data)
 
-    @try_handle_log_levels(log_mes='ERROR:{0}'.format(datetime.datetime.now()))
+    @try_handle_log_levels('Try to get data from file: ')
     def get(self):
         return self._repository.get()
 
-    @try_handle_log_levels(log_mes='ERROR:{0}'.format(datetime.datetime.now()))
+    @try_handle_log_levels('Try to get data by id from file: ')
     def get_by_id(self, id):
         return self._repository.get_by_id(id)
 
-    @try_handle_log_levels(log_mes='ERROR:{0}'.format(datetime.datetime.now()))
+    @try_handle_log_levels('Try to update data in file: ')
     def update(self, id, data):
         self._repository.update(id, data)
 
-    @try_handle_log_levels(log_mes='ERROR:{0}'.format(datetime.datetime.now()))
+    @try_handle_log_levels('Try to delete data in file: ')
     def delete(self, id):
         self._repository.delete(id)
 
