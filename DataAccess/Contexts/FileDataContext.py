1c4153673fb3ebb88e3fc7c0f45d80d1bf403879
AndersenGAV <Andersengal@gmail.com>
2024-01-22 15:47:29 +0200

    Type hints, refactoring, bugs fixing

--- DataAccess/Contexts/FileDataContext.py
+++ DataAccess/Contexts/FileDataContext.py
@@ -1,19 +1,18 @@
 import json
 import os
 
+from Configuration.config import DEFAULT_MATERIALS_DICT
 from Infrastructure.Validators.Decorators.FileExceptionHandlers import try_handle_log_levels
-
-from HardwareStore.Configuration.config import DEFAULT_MATERIALS_DICT
 
 
 class FileDataContext:
     __instance = None
-    __initialized = False
+    __initialized: bool = False
 
-    def __init__(self, path):
+    def __init__(self, path: str):
         if not self.__initialized:
             self._path = path
-            self.__connect()
+            self.__connect_of_default()
             FileDataContext.__initialized = True
 
     def __new__(cls, *args, **kwargs):
@@ -22,14 +21,14 @@ class FileDataContext:
         return cls.__instance
 
     @try_handle_log_levels("Connect to file")
-    def __connect_of_default(self):
+    def __connect_of_default(self) -> None:
         if not os.path.exists(self._path):
             with open(self._path, "w") as file:
-                json.dump(DEFAULT_MATERIALS_DICT,file)
+                json.dump(DEFAULT_MATERIALS_DICT, file)
 
     @property
-    def path(self):
+    def path(self) -> str:
         return self._path
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"{self.__class__}:{self._path}"
--- DataAccess/Repositories/MaterialFileRepository.py
+++ DataAccess/Repositories/MaterialFileRepository.py
@@ -8,40 +8,38 @@ class MaterialFileRepository:
     def __init__(self, context: FileDataContext):
         self._context = context
 
-    def add(self, data):
+    def add(self, data: dict) -> None:
         current_data = self.get()
         current_data[MATERIALS_JSON_KEY].append(data)
         self.__write(current_data)
 
-    def get(self):
-        try:
-            with open(self._context.path, "r", encoding=ENCODING_TYPE) as file:
-                return json.load(file)
-        except json.JSONDecodeError:
-            return {MATERIALS_JSON_KEY: []}
+    def get(self) -> dict:
+        with open(self._context.path, "r", encoding=ENCODING_TYPE) as file:
+            return json.load(file)
 
-    def get_by_id(self, id):
+    def get_by_id(self, id: int) -> dict:
         all_data = self.get()
-        MaterialFileRepository.__get_by_id_from(id, all_data)
+        return MaterialFileRepository.__get_by_id_from(id, all_data)
 
-    def update(self, id, data):
+    def update(self, id: int, data: int) -> None:
         all_data = self.get()
         updated_data = {
-            MATERIALS_JSON_KEY: list(map(lambda current: data if current[MATERIAL_ID] == id else current, all_data[MATERIALS_JSON_KEY]))}
+            MATERIALS_JSON_KEY: list(
+                map(lambda current: data if current[MATERIAL_ID] == id else current, all_data[MATERIALS_JSON_KEY]))}
         self.__write(updated_data)
 
-    def delete(self, id):
+    def delete(self, id: int) -> None:
         all_data = self.get()
         data_to_remove = MaterialFileRepository.__get_by_id_from(id, all_data)
         all_data.remove(data_to_remove)
         self.__write(all_data)
 
-    def __write(self, data):
+    def __write(self, data: dict) -> None:
         with open(self._context.path, "w", encoding=ENCODING_TYPE) as file:
             json.dump(data, file, indent=INDENT)
 
     @staticmethod
-    def __get_by_id_from(id, data):
+    def __get_by_id_from(id: int, data: dict) -> dict:
         for item in data[MATERIALS_JSON_KEY]:
             if item[MATERIAL_ID] == id:
                 return item
