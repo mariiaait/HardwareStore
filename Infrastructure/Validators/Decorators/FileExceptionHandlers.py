033464bcb6f866fa9274eb2bddec38429d8265b1
shana <bashkatovahelga@gmail.com>
2024-01-22 16:48:21 +0100

    Fixed decorator try_handle_log_levels

--- Infrastructure/Validators/Decorators/FileExceptionHandlers.py
+++ Infrastructure/Validators/Decorators/FileExceptionHandlers.py
@@ -13,16 +13,16 @@ def try_handle_log_levels(log_mes):
                 result = func(*args, **kwargs)
                 logger.info(f"INFO: {datetime.datetime.now()}\t{log_mes}\tValues: {args}, {kwargs}")
                 if func.__name__ not in ("add", "update", "delete"):
-                    return result
+                    return DataResponse(True, result, None)
             except FileNotFoundError as ex:
                 logger.error(f"ERROR: {datetime.datetime.now()}\t{ex}\tValues: {args}, {kwargs}")
-                return DataResponse(False, None, str(ex))
+                return DataResponse(False, None, ex)
             except json.JSONDecodeError as ex:
                 logger.error(f"ERROR: {datetime.datetime.now()}\t{ex}\tValues: {args}, {kwargs}")
-                return DataResponse(False, None, str(ex))
+                return DataResponse(False, None, ex)
             except Exception as ex:
                 logger.error(f"ERROR: {datetime.datetime.now()}\t{ex}\tValues: {args}, {kwargs}")
-                return DataResponse(False, None, str(ex))
+                return DataResponse(False, None, ex)
         return wrapper
 
     return decorator
