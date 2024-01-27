40895e3d3bcbdd2ff130f04a55eccbd516ab20a1
sergbash1991 <sergbash1991@gmail.com>
2024-01-27 14:22:08 +0200

    Added class TypeChecker. I added his realization in methods of Controller.

--- Infrastructure/Validators/Decorators/TypeChecker.py
+++ Infrastructure/Validators/Decorators/TypeChecker.py
@@ -6,25 +6,20 @@ from Configuration.config import RELATIVE_PATH_TO_LOG_FILE
 
 logging.basicConfig(filename=RELATIVE_PATH_TO_LOG_FILE, format='%(asctime)s - %(levelname)s - %(message)s')
 
-
-def type_check_decorator(func: Callable) -> Callable:
-    def wrapper(*args, **kwargs):
+class TypeChecker:
+    @staticmethod
+    def type_check(func: Callable, *args, **kwargs):
         type_hints = get_type_hints(func)
         parameters_in_order = inspect.signature(func).parameters
         pairs = zip(parameters_in_order, args)
+        TypeChecker.__check_zipped_types(type_hints, pairs)
+        TypeChecker.__check_zipped_types(type_hints, kwargs.items())
 
-        for parameter_name, parameter_value in pairs:
+
+    @staticmethod
+    def __check_zipped_types(type_hints: dict, data):
+        for parameter_name, parameter_value in data:
             parameter_type_hint = type_hints.get(parameter_name, None)
             if parameter_type_hint and not isinstance(parameter_value, parameter_type_hint):
                 raise TypeError(f"Type mismatch for argument {parameter_name}. "
                                 f"Expected {parameter_type_hint},got {type(parameter_value)}.")
-
-        for parameter_name, parameter_value in kwargs.items():
-            parameter_type_hint = type_hints.get(parameter_name, None)
-            if parameter_type_hint and not isinstance(parameter_value, parameter_type_hint):
-                raise TypeError(f"Type mismatch for argument {parameter_name}. "
-                                f"Expected {parameter_type_hint},got {type(parameter_value)}.")
-
-        return func(*args, **kwargs)
-
-    return wrapper
